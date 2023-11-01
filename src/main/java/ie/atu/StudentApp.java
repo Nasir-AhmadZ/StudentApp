aa95edea81392907a9336d43af544a49e7494b64
sagheerahmadGmit <37177234+sagheerahmadGmit@users.noreply.github.com>
2023-11-01 13:56:08 +0000

    Added Student 3

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -4,6 +4,7 @@ public class StudentApp {
     public static void main(String[] args) {
         Student student1 = new Student();
         Student student2 = new Student("");
+        Student student3 = new Student("","","");
 
         System.out.println("Enter info of first student: ");
         student1.studentInfo();
@@ -11,8 +12,14 @@ public class StudentApp {
         System.out.println("Enter info of second student: ");
         student2.studentInfo();
 
+        System.out.println("Enter info of third student");
+        student3.studentInfo();
 
+        System.out.println("Student 1\n");
         student1.displayInfo();
+        System.out.println("Student 2\n");
         student2.displayInfo();
+        System.out.println("Student 3\n");
+        student3.displayInfo();
     }
 }
