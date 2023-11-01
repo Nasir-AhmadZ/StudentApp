672c1c0fc98887debb712903482caf442ce10cbb
sagheerahmadGmit <37177234+sagheerahmadGmit@users.noreply.github.com>
2023-11-01 13:36:03 +0000

    added student1

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -2,6 +2,11 @@ package ie.atu;
 
 public class StudentApp {
     public static void main(String[] args) {
+        Student student1 = new Student();
 
+        System.out.println("Enter details of first student: ");
+        student1.studentInfo();
+
+        student1.displayInfo();
     }
 }
